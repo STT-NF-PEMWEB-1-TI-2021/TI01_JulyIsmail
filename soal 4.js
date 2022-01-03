a4fbddf4a537a339de64e157ee4da0c8e4eee7e0
JulyIsmail <92907910+JulyIsmail@users.noreply.github.com>
2022-01-03 13:37:49 +0700

    Delete soal 4.js

--- soal 4.js
+++
@@ -1,6 +0,0 @@
-function hitung(){
-    let b = Number(document.querySelector(".knj").value);
-    let t = Number(document.querySelector(".ksj").value);
-    let s = b + t;
-    document.querySelector(".myg").value=s;
-}
