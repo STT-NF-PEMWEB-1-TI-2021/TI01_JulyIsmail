aa22e538c1518f980aa8ad073655de25505c726f
JulyIsmail <92907910+JulyIsmail@users.noreply.github.com>
2022-01-03 14:00:03 +0700

    Add files via upload

---
+++ Tugas Jss 1/soal 4.js
@@ -0,0 +1,6 @@
+function hitung(){
+    let b = Number(document.querySelector(".knj").value);
+    let t = Number(document.querySelector(".ksj").value);
+    let s = b + t;
+    document.querySelector(".myg").value=s;
+}
